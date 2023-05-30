e166b19d2b1925cfe8dca37923ae0b558f2af1d0
Matt Oklahuman <theoklahuman@gmail.com>
2023-05-30 10:07:27 -0500

    Finish layout for mobile version

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./style.css";
+import "../assets/images/favicon-32x32.png";
+import image from "../assets/images/icon-arrow.svg";
+
+const arrowImage = document.getElementById("arrow-image");
+arrowImage.src = image;
+
